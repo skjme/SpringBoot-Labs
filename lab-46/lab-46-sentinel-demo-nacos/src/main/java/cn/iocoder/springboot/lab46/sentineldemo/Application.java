c6ddab88e0472d0bdea1e474cac486b9586fcce1
chaseshu <chase.shu@asia.training>
2020-08-17 16:41:38 +0800

    开启nacos发现服务

--- lab-46/lab-46-sentinel-demo-nacos/src/main/java/cn/iocoder/springboot/lab46/sentineldemo/Application.java
+++ lab-46/lab-46-sentinel-demo-nacos/src/main/java/cn/iocoder/springboot/lab46/sentineldemo/Application.java
@@ -2,7 +2,9 @@ package cn.iocoder.springboot.lab46.sentineldemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class Application {
 
